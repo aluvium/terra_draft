8bf46568f018408398bbb16037064c30613d2e14
aluvium <aluvium@protonmail.com>
2022-10-19 11:51:52 +0200

    vpc-elb-sg-vars

---
+++ terraform-aws-vpc-elb-sg-vars/main.tf
@@ -0,0 +1,85 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "var.aws_region"
+}
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.16.0"
+  cidr = "var.vpc_cidr_block"
+  azs             = data.aws_availability_zones.available.names
+  private_subnets = slice(var.private_subnet_cidr_blocks,0,var.private_subnet_count)
+  public_subnets  = slice(var.public_subnet_cidr_blocks,0,var.public_subnet_count)
+  enable_nat_gateway = true
+  enable_vpn_gateway = var.enable_vpn_gateway 
+  tags = var.resource_tags
+}
+
+module "app_security_group" {
+  source  = "terraform-aws-modules/security-group/aws//modules/web"
+  version = "4.0.0"
+  name        = "web-sg-${var.resource_tags["project"]}"
+  description = "Security group for web-servers with HTTP ports open within VPC"
+  vpc_id      = module.vpc.vpc_id
+  ingress_cidr_blocks = module.vpc.public_subnets_cidr_blocks
+  tags = var.resource_tags
+}
+
+module "lb_security_group" {
+  source  = "terraform-aws-modules/security-group/aws//modules/web"
+  version = "4.0.0"
+  name        = "lb-sg-${var.resource_tags["project"]}"
+  description = "Security group for load balancer with HTTP ports open within VPC"
+  vpc_id      = module.vpc.vpc_id
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  tags = var.resource_tags
+}
+
+resource "random_string" "lb_id" {
+  length  = 3
+  special = false
+}
+
+module "aws_elb" {
+  source  = "terraform-aws-modules/elb/aws"
+  version = "3.0.0"
+  name = "lb-${random_string.lb_id.result}-${var.resource_tags["project"]}"
+  internal = false
+  security_groups = ["lb-sg-${var.resource_tags["project"]}"]
+  subnets         = module.vpc.public_subnets
+  number_of_instances = length(module.ec2_instances.instance_ids)
+  instances           = module.ec2_instances.instance_ids
+  listener = [{
+    instance_port     = "80"
+    instance_protocol = "HTTP"
+    lb_port           = "80"
+    lb_protocol       = "HTTP"
+  }]
+  health_check = {
+    target              = "HTTP:80/index.html"
+    interval            = 10
+    healthy_threshold   = 3
+    unhealthy_threshold = 10
+    timeout             = 5
+  }
+  tags = var.resource_tags
+}
+
+module "ec2_instances" {
+  source = "./modules/aws-instance"
+  instance_count     = var.instance_count
+  instance_type      = var.ec2_instance_type
+  subnet_ids         = module.vpc.private_subnets[*]
+  security_group_ids = ["web-sg-${var.resource_tags["project"]}"]
+  tags = var.resource_tags
+}
---
+++ terraform-aws-vpc-elb-sg-vars/terraform.tfvars
@@ -0,0 +1,10 @@
+resource_tags = {
+  project     = "new-project",
+  environment = "test",
+  owner       = "aluvium@proton.me"
+}
+
+ec2_instance_type = "t2.micro"
+
+instance_count = 3
+
